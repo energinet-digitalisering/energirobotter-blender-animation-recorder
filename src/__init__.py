5324ccb715c0314e996e9e0fb365101ccc32dfca
Nicoline L. Thomsen <xnlth@energinet.dk>
2024-10-07 16:21:59 +0200

    Added addon test code

---
+++ src/__init__.py
@@ -0,0 +1,153 @@
+import os
+import platform
+import subprocess
+import sys
+
+import bpy
+from bpy.utils import register_class, unregister_class
+
+# Addon metadata
+bl_info = {
+    "name": "EnergirobotterBlenderAnimationRecorder",
+    "author": "Energinet",
+    "version": (0, 0, 0),
+    "blender": (4, 2, 1),
+    "location": "Toolbar > EnergirobotterBlenderAnimationRecorder",
+    "description": "Record angles of animation at each frame to CSV file.",
+    "category": "Animation",
+}
+
+
+# Non standard Python packages - "python import name": "pip install name"
+packages = {}
+
+
+def install_package(package):
+
+    if platform.system() == "win32":
+
+        python_exe = os.path.join(sys.prefix, "bin", "python.exe")
+        target = os.path.join(sys.prefix, "lib", "site-packages")
+
+        subprocess.call([python_exe, "-m", "ensurepip"])
+        subprocess.call([python_exe, "-m", "pip", "install", "--upgrade", "pip"])
+
+        subprocess.call(
+            [python_exe, "-m", "pip", "install", "--upgrade", package, "-t", target]
+        )
+
+    else:
+        subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+
+for package_py, package_pip in packages.items():
+    try:
+        exec("import " + package_py)
+
+    except ModuleNotFoundError:
+        print(
+            package_py
+            + " module not found, installing '"
+            + package_pip
+            + "' with pip..."
+        )
+
+        install_package(package_pip)
+        exec("import " + package_py)
+
+        print(package_py + " successfully imported")
+
+print("All packages installed")
+
+
+# Load addon modules
+from .animation_recorder import AnimationRecorder, State
+
+# Global objects
+recorder = AnimationRecorder()
+
+# Classes
+
+
+class SceneProperties(bpy.types.PropertyGroup):
+    # Defining custom properties to be used by the addon panel
+
+    ...
+
+
+class ANIMATIONRECORDER_OT_RecordAnimation(bpy.types.Operator):
+    # Go through animation timeline and get angles from rig, and record to file
+
+    bl_idname = "animation_recorder.record_animation"
+    bl_label = "Go through animation timeline and record angles"
+
+    def __init__(self):
+        print("Animation recording starting...")
+
+    def __del__(self):
+        print("Animation recording ended")
+
+    def modal(self, context, event):
+        if event.type == "ESC":
+            recorder.set_state_idle()
+
+            self.report({"INFO"}, "ESC key pressed, stopping animation recording")
+            return {"FINISHED"}
+
+        if recorder.state == State.IDLE:
+            self.report({"INFO"}, "Animation recording finished")
+            return {"FINISHED"}
+
+        return {"PASS_THROUGH"}
+
+    def invoke(self, context, event):
+        context.window_manager.modal_handler_add(self)
+
+        recorder.record_animation(self.report)
+
+        return {"RUNNING_MODAL"}
+
+
+class ANIMATIONRECORDER_PT_Panel(bpy.types.Panel):
+    # Addon panel displaying options
+
+    bl_label = "Animation Recorder"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = "AnimationRecorder"
+    bl_options = {"DEFAULT_CLOSED"}
+
+    def draw(self, context):
+        layout = self.layout
+
+        layout.row().operator(
+            ANIMATIONRECORDER_OT_RecordAnimation.bl_idname,
+            text="Record animation",
+            icon="PLAY",
+        )
+
+
+classes = (
+    SceneProperties,
+    ANIMATIONRECORDER_OT_RecordAnimation,
+    ANIMATIONRECORDER_PT_Panel,
+)
+
+
+def register():
+    for cls in classes:
+        register_class(cls)
+
+    bpy.types.Scene.scn_prop = bpy.props.PointerProperty(type=SceneProperties)
+
+
+def unregister():
+    for cls in classes:
+        unregister_class(cls)
+
+    del bpy.types.Scene.scn_prop
+
+
+if __name__ == "__main__":
+
+    register()
---
+++ src/animation_recorder.py
@@ -0,0 +1,105 @@
+from enum import Enum
+import mathutils
+import numpy as np
+
+import bpy
+
+
+class State(Enum):
+    IDLE = 0
+    RECORDING = 1
+
+
+class AnimationRecorder:
+
+    def __init__(self):
+
+        self.state = State.IDLE
+
+    def __del__(self):
+        self.set_state_idle()
+
+    def set_state_idle(self):
+        self.state = State.IDLE
+
+    # Helper functions from rigify plugin
+    def get_pose_matrix_in_other_space(self, mat, pose_bone):
+        """Returns the transform matrix relative to pose_bone's current
+        transform space. In other words, presuming that mat is in
+        armature space, slapping the returned matrix onto pose_bone
+        should give it the armature-space transforms of mat.
+        """
+
+        rest = pose_bone.bone.matrix_local.copy()
+        rest_inv = rest.inverted()
+
+        if pose_bone.parent.name != "Root":
+            par_mat = pose_bone.parent.matrix.copy()
+            par_inv = par_mat.inverted()
+            par_rest = pose_bone.parent.bone.matrix_local.copy()
+        else:
+            par_mat = mathutils.Matrix()
+            par_inv = mathutils.Matrix()
+            par_rest = mathutils.Matrix()
+
+        # Get matrix in bone's current transform space
+        smat = rest_inv @ (par_rest @ (par_inv @ mat))
+
+        return smat
+
+    def get_bones_rotation(self, pose_bone, axis):
+
+        mat = self.get_pose_matrix_in_other_space(pose_bone.matrix, pose_bone)
+
+        if axis == 0:
+            return mat.to_euler().x
+        elif axis == 1:
+            return mat.to_euler().y
+        elif axis == 2:
+            return mat.to_euler().z
+
+    def angle_of_bone(self, bone):
+
+        # Get unconstrained axis
+        axis_rot = (np.array(bone.lock_rotation) == False).nonzero()[0][0]
+
+        return np.rad2deg(self.get_bones_rotation(bone, axis_rot))
+
+    def angles_of_bones(self):
+        for bone in bpy.context.active_object.pose.bones:
+
+            if bone.name == "Root":
+                continue
+
+            angle = self.angle_of_bone(bone)
+            print(bone.name + ": " + str(angle))
+
+    def record_animation(self, report_blender):
+
+        if not self.state == State.RECORDING:
+            self.state = State.RECORDING
+
+            bpy.data.scenes["Scene"].frame_set(0)
+
+            # Iterate through all animation frames
+            while self.state == State.RECORDING:
+                bpy.data.scenes["Scene"].frame_set(
+                    bpy.data.scenes["Scene"].frame_current + 1
+                )
+
+                # Record data
+                print(bpy.data.scenes["Scene"].frame_current)
+                self.angles_of_bones()
+
+                if (
+                    bpy.data.scenes["Scene"].frame_current
+                    >= bpy.data.scenes["Scene"].frame_end
+                ):
+                    break
+
+            report_blender({"INFO"}, "End of animation")
+
+            self.state = State.IDLE
+
+        else:
+            report_blender({"INFO"}, "Animation is already in progress")
